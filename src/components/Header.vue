e2212174a97ab3c653966114355650a8cafc54fd
mohsen <mohsen.zahraee123445@gmail.com>
2021-10-12 00:46:55 +0330

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
             <p class="text-base font-black font-IRANSans">دیکشنری عربی نبراس</p>
             <p class="text-sm font-Yekan">لهجه <span class="bg-gray-300 rounded-lg px-1"> عراقی</span> و <span class="px-1 bg-gray-300 rounded-lg">خلیجی</span></p>
         </div>
-        <span class="">منو</span>
+        <span class="text-xl"><fa icon="align-justify" /></span>
     </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faAlignJustify)
 
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).component('fa', FontAwesomeIcon).use(router).mount('#app')
