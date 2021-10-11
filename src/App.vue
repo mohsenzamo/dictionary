4573441ed6073669b624b2093b2324c5ecb8ff13
mohsen <mohsen.zahraee123445@gmail.com>
2021-10-11 20:38:52 +0330

    install tail wind

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/logo.png" class="bg-yellow-300"/>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
