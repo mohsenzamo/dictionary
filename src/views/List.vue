a99481aff30f30d106ce7d946e5ab23864732c6e
ArminBM <armin.bayat.m2777@gmail.com>
2021-10-13 18:58:59 +0330

    sarting the design of the list page

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,3 +1,33 @@
 <template>
-  page list
+  <div
+    class="grid grid-rows-9 h-screen bg-gray-100 pt-16 gap-x-8 gap-y-2 justify-items-stretch"
+  >
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      1
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      2
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      3
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      4
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      5
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      6
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      7
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      8
+    </div>
+    <div class="bg-red-500 row-span-1 rounded-lg active:-translate-y-1">
+      9
+    </div>
+  </div>
 </template>
