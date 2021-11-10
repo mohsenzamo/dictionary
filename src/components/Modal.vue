59a2aeb9fdb459f72b14bd849596b4f9220f3877
mohsen <mohsen.zahraee123445@gmail.com>
2021-11-10 15:58:23 +0330

    responsive modal component and searchLoader component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,13 +3,13 @@
 
 </script>
 <template>
-  <div class="modal-page">
+  <div class="fixed inset-0 flex items-center justify-center z-50">
     <div
-      class="modal-bg-box"
+      class="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-70"
       @click="$emit('close')"
     />
     <div
-      class="modal-box"
+      class="w-10/12 bg-gray-300 rounded-xl shadow-2xl relative animate-open p-6 h-auto lg:w-6/12"
     >
       <slot />
       <button
@@ -31,7 +31,6 @@
 .modal-enter-to {
   transition: all .5s ease;
 }
-
 .modal-leave-from {
   opacity: 1;
 }
@@ -41,14 +40,4 @@
 .modal-leave-active {
   transition: all .5s ease;
 }
-.modal-bg-box{
-  @apply fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-70
-}
-.modal-box{
-  @apply w-10/12 bg-gray-300 rounded-xl shadow-2xl relative animate-open p-6 h-auto lg:w-6/12
-}
-.modal-page{
-  @apply fixed inset-0 flex items-center justify-center z-50
-}
-
 </style>
