55a72037bb8ff5a13f88f381aa4dfda0b214a786
mohsen <mohsen.zahraee123445@gmail.com>
2021-11-10 13:56:39 +0330

    responsive AboutUs page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,58 +2,48 @@
 import { computed, ref, watch } from 'vue'
 import backHeader from '../components/BackHeader.vue'
 import HeaderLarge from '../components/HeaderLarge.vue'
-const screenWidth = ref(window.screen.width)
+const mediaMatcher = matchMedia('(max-width: 1024px)')
+const laptopScreen = ref(mediaMatcher.matches)
+mediaMatcher.addListener(() => {
+  laptopScreen.value = !laptopScreen.value
+})
 </script>
 <template>
-  <backHeader v-if="screenWidth < 1024">
+  <backHeader v-if="laptopScreen">
     درباره ما
   </backHeader>
   <HeaderLarge v-else />
-  <div class="blur" />
-  <div class="about-box">
+  <div class="bg-logo w-screen h-screen bg-contain bg-no-repeat bg-opacity-75 bg-top blur-sm opacity-70 lg:w-full" />
+  <div class="absolute top-44 grid w-screen text-center justify-items-center font-IRANSans cursor-default lg:w-full">
     <img
       src="../assets/logo.png"
-      class="w-32 xl:w-72"
+      class="w-32 lg:w-72"
     >
     <div class="-mt-4 mb-4">
-      <p class="font-bold xl:text-2xl">
+      <p class="font-bold lg:text-2xl">
         نبراس
       </p>
-      <p class="text-sm xl:text-xl">
+      <p class="text-sm lg:text-xl">
         دیکشنری عربی به فارسی
       </p>
     </div>
     <div
-      class="px-5 w-screen xl:w-5/12"
+      class="px-5 w-screen lg:w-5/12"
       style="direction: ltr;"
     >
-      <p class="instagram">
+      <p class="w-full bg-red-600 rounded-3xl h-10 flex items-center pl-4 gap-2 text-white mb-4 select-all lg:h-14 lg:rounded-full lg:text-xl">
         <fa :icon="['fab','instagram']" />
         @Nebras_arabic
       </p>
-      <p class="telegram">
+      <p class="w-full bg-blue-600 rounded-3xl h-10 flex items-center pl-4 gap-2 text-white mb-4 select-all lg:h-14 lg:rounded-full lg:text-xl">
         <fa :icon="['fab','telegram']" />
         @Nebras_arabic
       </p>
     </div>
     <div class="px-5">
-      <p class="text-sm text-center xl:text-xl">
+      <p class="text-sm text-center lg:text-xl">
         کلیه حقوق این نرم افزار متعلق به گروه آموزشی نبراس است<br> و هرگونه کپی برداری از آن بدون ذکر منبع شرعا و قانونا ممنوع است.
       </p>
     </div>
   </div>
 </template>
-<style>
-.blur{
-  @apply bg-logo w-screen h-screen bg-contain bg-no-repeat bg-opacity-75 bg-top blur-sm opacity-70 xl:w-full
-}
-.about-box{
-  @apply absolute top-44 grid w-screen text-center justify-items-center font-IRANSans cursor-default xl:w-full
-}
-.instagram{
-  @apply w-full bg-red-600 rounded-3xl h-10 flex items-center pl-4 gap-2 text-white mb-4 select-all xl:h-14 xl:rounded-full xl:text-xl
-}
-.telegram{
-  @apply w-full bg-blue-600 rounded-3xl h-10 flex items-center pl-4 gap-2 text-white mb-4 select-all xl:h-14 xl:rounded-full xl:text-xl
-}
-</style>
